c4a5b16744812f84c7edcf9d54ba98c41765efcc
Marcelo Cohen <mflashbr@gmail.com>
2025-05-08 09:25:22 -0300

    DFS quase completo (Java)

--- DFS.java
+++ DFS.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class DFS {
@@ -6,17 +10,34 @@ public class DFS {
     private Graph g;
     private String inicial;
     private Set<String> marked;
+    private Map<String, String> edgeTo;
 
     public DFS(Graph g, String inicial) {
         this.g = g;
         this.inicial = inicial;
         marked = new HashSet<>();
+        edgeTo = new HashMap<>();
         dfs(inicial);
     }
 
     private void dfs(String v) {
         System.out.println("Entrando: " + v);
+        marked.add(v);
+        for (String w : g.getAdj(v)) {
+            // Se não estiver marcado, visita
+            if (!marked.contains(w))
+                dfs(w);
+        }
+        System.out.println("Saindo: " + v);
+    }
 
+    public boolean hashPathTo(String v) {
+        // retorno true se tiver caminho para v
+        return false;
+    }
+
+    public List<String> pathTo(String v) {
+        return new ArrayList<>();
     }
 
     public static void main(String[] args) {

b6af9b44e80429973448c240f27d8747d2cea110
Marcelo Cohen <mflashbr@gmail.com>
2025-05-13 09:07:21 -0300

    DFS completo em Java

--- DFS.java
+++ DFS.java
@@ -25,23 +25,47 @@ public class DFS {
         marked.add(v);
         for (String w : g.getAdj(v)) {
             // Se não estiver marcado, visita
-            if (!marked.contains(w))
+            if (!marked.contains(w)) {
+                // indica que para chegar em w
+                // viemos por v
+                edgeTo.put(w, v);
                 dfs(w);
+            }
         }
         System.out.println("Saindo: " + v);
     }
 
     public boolean hashPathTo(String v) {
         // retorno true se tiver caminho para v
-        return false;
+        return marked.contains(v);
+        // if(marked.contains(v))
+        // return true;
+        // return false;
     }
 
     public List<String> pathTo(String v) {
-        return new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        if (!hashPathTo(v))
+            return path;
+        while (!v.equals(inicial)) {
+            path.add(0, v);
+            v = edgeTo.get(v);
+        }
+        path.add(0, inicial);
+        return path;
     }
 
     public static void main(String[] args) {
         Graph g = new Graph("exemplos/tinyG.txt");
         DFS dfs = new DFS(g, "0");
+
+        for (String v : g.getVerts()) {
+            System.out.print(v + ": ");
+            if (!dfs.hashPathTo(v))
+                System.out.println("Não tem caminho");
+            else {
+                System.out.println(dfs.pathTo(v));
+            }
+        }
     }
 }

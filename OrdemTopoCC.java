db7f0cc98bdca62bcb376519ecd5cdd03a547d75
Marcelo Cohen <mflashbr@gmail.com>
2025-05-22 08:20:31 -0300

    Exemplo de ordem topológica: matriz do CC

--- OrdemTopoCC.java
+++ OrdemTopoCC.java
@@ -1,21 +1,30 @@
-public class OrdemTopoCC
-{
+public class OrdemTopoCC {
     public static void main(String[] args) {
         Digraph g = new Digraph();
         // Ler cada linha do arquivo exemplo/curriculo_98aj.txt
         // Em cada linha (strings separadas por "/")
-        //     - Primeira string é a disciplina destino
-        //     - Próximas strings são os pré-requisitos
+        // - Primeira string é a disciplina destino
+        // - Próximas strings são os pré-requisitos
         // Gerar uma aresta de cada pré-requisito para a disciplina
+        In arq = new In("exemplos/curriculo_98aj.txt");
+        while (arq.hasNextLine()) {
+            String linha = arq.readLine();
+            String[] aux = linha.split("/");
+            String destino = aux[0];
+            for (int pos = 1; pos < aux.length; pos++) {
+                String origem = aux[pos];
+                g.addEdge(origem, destino);
+            }
+        }
 
         // Gerar o grafo em formato dot (opcional,
         // para facilitar a visualização)
-        // System.out.println(g.toDot());
+        System.out.println(g.toDot());
 
         // Executar ordenação topológica
         OrdemTopologica ot = new OrdemTopologica(g);
-        for(String v: ot.getOrdemTopologica()) {
-            System.out.println(v+", ");
+        for (String v : ot.getOrdemTopologica()) {
+            System.out.println(v + ", ");
         }
     }
 }
